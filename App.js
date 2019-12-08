91c9bd8b8307ecdb0a3fcb829b87c41716df1833
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-09 01:50:35 +0300

    Timing and Spring animated functions added

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Animated } from 'react-native'
 import styled from 'styled-components/native'
-import Tabs from './Tabs'
-import Stack from './Stack'
-import Drawer from './Drawer'
+
+// decay - initial velocity and slow to a stop
+// spring - spring physics model
+// timing - animated a value over time
 
 const CenterView = styled.View`
   flex: 1;
@@ -27,21 +28,64 @@ const WelcomeBtn = styled.TouchableOpacity`
 `
 
 export default class App extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+    width: new Animated.Value(0),
+    height: new Animated.Value(0),
+  }
+
+  componentDidMount () {
+    const { opacity, width, height } = this.state
+
+    Animated.timing(opacity, { toValue: 1, duration: 1000 }).start()
+
+    Animated.spring(width, { toValue: 300, speed: 5 }).start()
+
+    Animated.spring(height, { toValue: 300, speed: 5 }).start()
+  }
+
   render () {
+    const { opacity, width, height } = this.state
+    
     return (
-      <CenterView>
-        <WelcomeText>
-          Different!
-        </WelcomeText>
-        <WelcomeBtn onPress={() => alert('Hello!')}>
-          <WelcomeText>
-            Push Me!
-          </WelcomeText>
-        </WelcomeBtn>
-      </CenterView>
+      <View style={styles.container}>
+        <Animated.Image
+          style={[styles.img, { opacity, width, height }]}
+          source={{ uri: 'https://tylermcginnis.com/tylermcginnis_glasses-300.png' }}
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img: {
+    width: 200,
+    height: 200,
+  }
+})
+
+// export default class App extends Component {
+//   render () {
+//     return (
+//       <CenterView>
+//         <WelcomeText>
+//           Different!
+//         </WelcomeText>
+//         <WelcomeBtn onPress={() => alert('Hello!')}>
+//           <WelcomeText>
+//             Push Me!
+//           </WelcomeText>
+//         </WelcomeBtn>
+//       </CenterView>
+//     )
+//   }
+// }
 
 // class FlexboxExamples extends Component {
 //   render() {
